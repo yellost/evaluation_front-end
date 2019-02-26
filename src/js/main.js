5396f9efef4a887206730798ffe8f2196e6bb957
Benoit <Benoitfranck2001@yahoo.fr>
2019-02-26 12:13:15 +0100

    mid 2nd day

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,7 @@ $(".burger").click(function() {
 $("li.mask").click(function () {
     $(".burger").toggleClass("cross");
     $(".main-nav>ul").toggleClass("hidden");
+    $("body").css('overflow','auto');
 })
 
 //---------------FORMS TOGGLE-------------//
@@ -34,3 +35,36 @@ $(".tab").click(function() {
         $("."+this.id).addClass("show");
     }
 });
+
+
+//------------------SWIPER-------------------//
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+          },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+      });
+  });
+
+//------------------AXIOS & VUE-------------------//
+new Vue({
+    el: "#app",
+    data: {
+      travelList: [],
+      totalPrice: 0,
+      orderTypes: 0,
+      sent: false
+    },
+    created() {
+      axios.get("https://wt-4abc83e5c2056740a9e00a6e0975a49a-0.sandbox.auth0-extend.com/city-trip")
+        .then(response => {
+          this.travelList = response.data
+        })
+    },
+})
